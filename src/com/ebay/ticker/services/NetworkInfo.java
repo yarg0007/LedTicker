193bf380d27e179477f3e86ba91c319e6b6c548b
Ben Yarger <byarger@asbtechnologies.com>
2018-05-28 08:27:23 -0700

    NetworkInfo reporter.

---
+++ src/com/ebay/ticker/services/NetworkInfo.java
@@ -0,0 +1,68 @@
+package com.ebay.ticker.services;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Collections;
+import java.util.Enumeration;
+
+public class NetworkInfo implements ServiceOutput, ServiceState {
+
+	static final String[] networkPrefixes = new String[] {"et", "en"};
+
+	public static void main(String args[]) throws SocketException {
+		NetworkInfo networkInfo = new NetworkInfo();
+		System.out.println(networkInfo.getTickerMessage());
+	}
+
+	@Override
+	public void startService() {
+
+	}
+
+	@Override
+	public void stopService() {
+
+	}
+
+	@Override
+	public String getTickerMessage() {
+		return displayInterfaceInformation();
+	}
+
+	private String displayInterfaceInformation() {
+
+		StringBuilder info = new StringBuilder();
+
+		Enumeration<NetworkInterface> nets = null;
+		try {
+			nets = NetworkInterface.getNetworkInterfaces();
+		} catch (SocketException e) {
+			return "Unable to get network interfaces. - SocketException.";
+		}
+
+		for (NetworkInterface netint : Collections.list(nets)) {
+
+			for (String prefix : networkPrefixes) {
+				if (netint.getName().startsWith(prefix)) {
+					info.append(displayInterfaceInformation(netint));
+					info.append("        ");
+					break;
+				}
+			}
+		}
+
+		return info.toString();
+	}
+
+	private String displayInterfaceInformation(NetworkInterface netint) {
+
+		StringBuilder stringBuilder = new StringBuilder();
+		stringBuilder.append(String.format("Display name: %s ", netint.getDisplayName()));
+		Enumeration<InetAddress> inetAddresses = netint.getInetAddresses();
+		for (InetAddress inetAddress : Collections.list(inetAddresses)) {
+			stringBuilder.append(String.format("- InetAddress: %s", inetAddress));
+		}
+		return stringBuilder.toString();
+	}
+}
